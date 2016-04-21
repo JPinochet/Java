323d67e290016fc82ccba24f32b230a3d151bc49
JPinochet <jorgepinochet@shaw.ca>
2016-04-20 19:06:10 -0600

    School Java Assignment

---
+++ BattleshipAssignment/src/serverSide/Server.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package serverSide;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+/**
+ * @author 432873
+ *
+ */
+public class Server
+{
+	public static void main(String[] args)
+	{
+		ArrayList<Socket> sockets = new ArrayList<Socket>(5);
+		try
+		{
+			ServerSocket ss = new ServerSocket(4444);
+			System.out.println("Server ready for all comers!!!!");
+			while(true)
+			{
+				Socket cs = ss.accept();
+				System.out.println("Connection accepted from " + cs.getInetAddress());
+				sockets.add(cs);
+				
+				if(sockets.size() == 2)
+				{
+					ClientHandler ch = new ClientHandler(sockets.get(0),sockets.get(1));
+					ch.start();
+					sockets.clear();
+				}
+				
+				System.out.println("Waiting for next client!");
+			}
+		}
+		catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ BattleshipAssignment/src/serverSide/ServerRunner.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package serverSide;
+
+import java.io.IOException;
+
+/**
+ * @author 432873
+ *
+ */
+public class ServerRunner
+{
+
+	/**
+	 * @param args
+	 */
+	@SuppressWarnings("static-access")
+	public static void main(String[] args)
+	{
+		Server chatServer = new Server();
+		chatServer.main(args);
+		
+		BSServer bsServer = new BSServer();
+		try
+		{
+			bsServer.main(args);
+		}
+		catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
